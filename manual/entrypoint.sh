4f7b258d396026fde607a64610b571d78b39224a
sandeebh <73829502+sandeebh@users.noreply.github.com>
2020-11-03 16:45:16 +0530

    Update entrypoint.sh

--- manual/entrypoint.sh
+++ manual/entrypoint.sh
@@ -21,7 +21,7 @@ ACTIONS_RUNNER_INPUT_NAME=$HOSTNAME
 ACTIONS_RUNNER_INPUT_TOKEN="$(curl -sS --request POST --url "https://github.com/pandind/gcp_runners/actions/runners/registration-token" --header "authorization: Bearer AQSHWARKOD73VSKOCSGRH6K7UFDW2"  --header 'content-type: application/json' | jq -r .token)"
 # configure runner
 export RUNNER_ALLOW_RUNASROOT=1
-/runner/config.sh --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$ACTIONS_RUNNER_INPUT_TOKEN" --labels k8s-runner
+./config.sh --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$ACTIONS_RUNNER_INPUT_TOKEN" --labels k8s-runner
 # start runner
 # https://github.com/actions/runner/issues/246#issuecomment-615293718
-/runner/bin/runsvc.sh
+./runsvc.sh
