94f00d121f310c768db15690df793f5ffb1e76dc
sandeepraj bhandari <73886155+sandeepDB@users.noreply.github.com>
2020-11-04 14:04:30 +0530

    Update entrypoint.sh

--- manual/entrypoint.sh
+++ manual/entrypoint.sh
@@ -18,10 +18,11 @@
 # ACTIONS_RUNNER_INPUT_NAME is used by config.sh
 ACTIONS_RUNNER_INPUT_NAME=$HOSTNAME
 # get regsistration token for this runnner
-ACTIONS_RUNNER_INPUT_TOKEN="$(curl -sS --request POST --url "https://github.com/pandind/gcp_runners/actions/runners/registration-token" --header "authorization: Bearer AQSHWARKOD73VSKOCSGRH6K7UFDW2"  --header 'content-type: application/json' | jq -r .token)"
+#ACTIONS_RUNNER_INPUT_TOKEN="$(curl -sS --request POST --url "https://github.com/pandind/gcp_runners/actions/runners/registration-token" --header "authorization: Bearer AQSHWARKOD73VSKOCSGRH6K7UFDW2"  --header 'content-type: application/json' | jq -r .token)"
 # configure runner
 export RUNNER_ALLOW_RUNASROOT=1
-./config.sh --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$ACTIONS_RUNNER_INPUT_TOKEN" --labels k8s-runner
+#./config.sh --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$ACTIONS_RUNNER_INPUT_TOKEN" --labels k8s-runner
+./config.sh --url https://github.com/pandind/gcp_runners --token AQSHWAXSDQ6FV4TYON5W6GC7UJROW
 # start runner
 # https://github.com/actions/runner/issues/246#issuecomment-615293718
 ./runsvc.sh
